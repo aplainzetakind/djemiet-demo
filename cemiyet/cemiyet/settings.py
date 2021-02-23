f6e8938b2bf3b808963e7daa36db19104d708290
mge <a@a.a>
2021-02-23 11:54:09 +0100

    static fix

--- cemiyet/cemiyet/settings.py
+++ cemiyet/cemiyet/settings.py
@@ -123,7 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
+#STATIC_ROOT = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
