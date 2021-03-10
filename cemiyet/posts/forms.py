4f30c33453c93c4e1f7aa0d51492f08020e9a245
aplainzetakind <thisisnot@valid.org>
2021-03-11 02:34:00 +0300

    Posting page working.

--- cemiyet/posts/forms.py
+++ cemiyet/posts/forms.py
@@ -6,9 +6,3 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'body', 'tags')
-
-# Not sure about this part
-#    def __init__(self, *args, **kwargs):
-#        user = kwargs.pop('user','')
-#        super(PostForm, self).__init__(*args, **kwargs)
-#        self.fields['tags']=forms.ModelChoiceField(queryset=Tag.objects.all())
--- cemiyet/posts/views.py
+++ cemiyet/posts/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Post, Tag
 from .forms import PostForm
 
-# Create your views here.
-
-
-# Built by following https://djangocentral.com/creating-comments-system-with-django/
-# I think post page should eventually be a popup rather than a separate page you navigate to --KBA
 def post(request):
     template_name = 'post.html'
 
@@ -16,13 +12,13 @@ def post(request):
             # Create Comment object but don't save to database yet
             new_post = post_form.save(commit=False)
             # Assign the current post to the comment
-            new_post.title = post_form.fields["title"]
-            new_post.body = post_form.fields["body"]
-            new_post.tags = post_form.fields["tags"]  # Error here
-#            new_post.tags = Tag.objects.get(id=tag_name)
-#            new_post.refs = post_form.refs
-            # Save to the database
+            new_post.title = post_form.cleaned_data["title"]
+            new_post.body = post_form.cleaned_data["body"]
+            new_post.tags = post_form.cleaned_data["tags"]
             new_post.save()
+
+            return HttpResponseRedirect('/content')
+
     else:
         post_form = PostForm()
 
