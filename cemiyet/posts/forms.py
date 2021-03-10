699596cbc2db7f02256bc7eafe18f16bb867184b
kbasaran <kbasaran@gmail.com>
2021-03-10 22:43:09 +0100

    Post form preliminary
    with error in tags

--- cemiyet/posts/forms.py
+++ cemiyet/posts/forms.py
@@ -5,4 +5,10 @@ from django import forms
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('tags', 'refs', 'body')
+        fields = ('title', 'body', 'tags')
+
+# Not sure about this part
+#    def __init__(self, *args, **kwargs):
+#        user = kwargs.pop('user','')
+#        super(PostForm, self).__init__(*args, **kwargs)
+#        self.fields['tags']=forms.ModelChoiceField(queryset=Tag.objects.all())
--- cemiyet/posts/views.py
+++ cemiyet/posts/views.py
@@ -16,9 +16,11 @@ def post(request):
             # Create Comment object but don't save to database yet
             new_post = post_form.save(commit=False)
             # Assign the current post to the comment
-            new_post.tags = post_form.tags
-            new_post.refs = post_form.refs
-            new_post.body = post_form.body
+            new_post.title = post_form.fields["title"]
+            new_post.body = post_form.fields["body"]
+            new_post.tags = post_form.fields["tags"]  # Error here
+#            new_post.tags = Tag.objects.get(id=tag_name)
+#            new_post.refs = post_form.refs
             # Save to the database
             new_post.save()
     else:
