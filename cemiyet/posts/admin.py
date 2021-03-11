839064bc5c9c7d1ae2d6e9f940a068f5ca9217ce
MuratY <yilmaz_murat@outlook.com.tr>
2021-03-11 10:43:08 +0300

    autocomplete icin taglere searchfield eklendi postadminde tag field autocomplete yapildi

--- cemiyet/posts/admin.py
+++ cemiyet/posts/admin.py
@@ -4,9 +4,12 @@ from .models import Post,Tag
 #admin liste gorunumu
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'author', 'tags')
+    autocomplete_fields = ['tags']
 
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ['name']
 #admin.site.register(Post)
 
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Tag)
+admin.site.register(Tag, TagAdmin)
