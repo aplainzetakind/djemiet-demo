5e592360e9b7a39314b1a612bf05e84c965b32b5
aplainzetakind <thisisnot@valid.org>
2021-03-15 01:02:00 +0300

    Registration page; tokens destroyed after use; random token generator.

--- cemiyet/tokens/models.py
+++ cemiyet/tokens/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from random import choice
+from string import ascii_letters
 
 # Create your models here.
+
 class RegistrationToken(models.Model):
-    token = models.CharField(max_length=12, unique=True)
+    token = models.CharField(max_length=8, unique=True)
+
+    def __str__(self):
+        return self.token
+
+def new_token():
+    tkn = ''.join([ choice(ascii_letters) for _ in range(8)])
+    return RegistrationToken.objects.create(token=tkn)
--- cemiyet/tokens/views.py
+++ cemiyet/tokens/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import HttpResponseNotFound
 from django.views.generic.edit import FormView
-#  from django.views import View
-#  from .models import RegistrationToken
+from .models import RegistrationToken
 from django.contrib.auth.forms import UserCreationForm
 from django.views.defaults import page_not_found
+from django.shortcuts import redirect
 
 class TokenRegView(FormView):
-    # Need redirection logic here.
-    #
-    # * If an actually registered user lands in this
-    # view, they should get some sort of 'you are already logged in' message.
-    #
-    # * If the token name dispatched from the url '/reg/<token>' does not exist
-    # in the RegistrationToken table of the database, we should 404.
-    #
-    # I havent figured out how to insert such a decision into this class-based
-    # view.
     form_class = UserCreationForm
     template_name = 'register.html'
     success_url = '/'
 
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+    def get(self, request, token):
+        if request.user.is_authenticated:
+            return redirect('/')
+        query = RegistrationToken.objects.filter(token=token)
+        if query.exists():
+            return super().get(request, token)
+        return HttpResponseNotFound('You have been misled.')
+
+    def post(self, request, token):
+        form = self.get_form()
+        if form.is_valid():
+            form.save()
+            RegistrationToken.objects.filter(token=token).first().delete()
+            return self.form_valid(form)
+        return self.form_invalid(form)
