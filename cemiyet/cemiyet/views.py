e92d5687db78bc2e44b2272ea20e098568263ef2
Me19 <metegeles@gmail.com>
2021-03-15 22:26:42 +0100

    (INIT) added Tag Filtering with Tag Links

--- cemiyet/cemiyet/views.py
+++ cemiyet/cemiyet/views.py
@@ -16,6 +16,19 @@ def auth_wall(request):
 
 @method_decorator(login_required, name='dispatch')
 class PostList(generic.ListView):
-    queryset = Post.objects.order_by('-created_on')
+    def get_queryset(self):
+        queryset = Post.objects.all()
+
+        if self.request.GET.get('t'):
+            queryset = queryset.filter(tags__name=self.request.GET.get('t'))
+        return queryset
+
+
+        # qs = Post.objects.order_by('-created_on')
+        # if self.request.GET.get('t'):
+        #     return Post.objects.filter(tag__name__iexact=self.request.GET.get['t'])
+        # return qs.order_by('-created_on')
+
+#    queryset = Post.objects.order_by('-created_on')
     paginate_by = settings.POSTS_COUNT_PER_PAGE
     template_name = 'content.html'
