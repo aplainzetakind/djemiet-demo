4e36b1ee0b4124594188402f2fa6968471ccf91b
MuratY <yilmaz_murat@outlook.com.tr>
2021-03-22 21:12:54 +0300

    auto

--- cemiyet/posts/forms.py
+++ cemiyet/posts/forms.py
@@ -11,19 +11,28 @@ class PostForm(forms.ModelForm):
         #  but feels awkward since in principle a user can POST that field by
         #  overriding stuff.  But since we ignore whatever would be posted and
         #  overwrite it, that shouldn't be a problem.
-        fields = ('title', 'body', 'tags', 'parents')
+        fields = ('title', 'body', 'parents', 'tags')
 
     tags = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.all(),
     )
+
+    tag_text=forms.CharField()
 
     def clean(self):
         cd = self.cleaned_data
 
         body = cd['body']
         title= cd['title']
+        tt=cd['tag_text']
         try:
             cd['parents'] = get_refs(title) + get_refs(body)
 
         except Post.DoesNotExist:
             raise forms.ValidationError("Non-existent reference.")
+
+        try:
+            cd['tags'] = Tag(name=tt)
+
+        except Tag.DoesNotExist:
+            raise forms.ValidationError("Non-existent reference.")        
