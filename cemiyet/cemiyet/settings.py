da9e5ba3102258c4c416750fb813ff13466adf0b
aplainzetakind <thisisnot@valid.org>
2021-05-09 15:59:16 +0300

    Better mobile layout

--- cemiyet/cemiyet/settings.py
+++ cemiyet/cemiyet/settings.py
@@ -32,14 +32,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'posts',
-    'tokens',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
+    'tokens',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
