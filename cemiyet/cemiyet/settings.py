f905a6e6753004801abb3aa40e409165393a0e23
aplainzetakind <thisisnot@valid.org>
2021-05-12 17:15:11 +0300

    Increase posts per page

--- cemiyet/cemiyet/settings.py
+++ cemiyet/cemiyet/settings.py
@@ -137,7 +137,7 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # Paging
-POSTS_COUNT_PER_PAGE = 9
+POSTS_COUNT_PER_PAGE = 18
 
 
 # Import settings from '../local_settings.py' if available.
